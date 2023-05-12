88d9df734d002c5c451a22e348b690042db3ecf3
Alexei Leukhin <Panzerwaffe@users.noreply.github.com>
2023-05-12 16:44:18 +0300

    special error added

--- p22/tests/test_crud.py
+++ p22/tests/test_crud.py
@@ -1,6 +1,6 @@
 def test_create(app, client):
     res = client.get('/create')
-    assert res.status_code == 200
+    assert res.status_code == 404 # jsut example to drop the test
 
     data = {"text": "primer"}
     res = client.post('/create', data=data)
